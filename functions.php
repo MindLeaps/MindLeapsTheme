a8c292f4b12310e58020a45fec3ea98389f65245
Tomislav Pesut <tpesut@mindleaps.org>
2015-06-03 21:56:43 -0400

    Added endpoint for Kurema Classes

--- functions.php
+++ functions.php
@@ -8,3 +8,37 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
 }
+
+function get_kurema_classes() {
+	$args = array(
+		'post_type' => 'class',
+		'numberposts' => -1
+	);
+
+
+	$classes = get_posts($args);
+	$ret = array();
+	foreach ($classes as $class) {
+		$class_data = array();
+
+		$class_data['country'] = get_post_meta($class->ID, 'wpcf-country', TRUE);
+		if ($class_data['country'] === 'US') {
+			$class_data['country'] = $class_data['country'].'-'.get_post_meta($class->ID, 'wpcf-state', TRUE);
+		}
+
+		$class_data['title'] = $class->post_title;
+		$class_data['description'] = wpautop($class->post_content);
+
+		array_push($ret, $class_data);
+
+	}
+
+	header('Content-type: application/json');
+	header('Access-Control-Allow-Origin: *');
+	echo json_encode($ret);
+	die();  
+
+}
+
+add_action('wp_ajax_get_kurema_classes', 'get_kurema_classes');
+add_action('wp_ajax_nopriv_get_kurema_classes', 'get_kurema_classes');
